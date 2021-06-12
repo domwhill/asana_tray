3966cb4f815c08d0d0826139b1ca2f490fd35dcc
domwhill <dominicwhill@gmail.com>
2021-06-12 18:54:13 +0100

    Authorise using the refresh token if it exists

--- oauth/main.js
+++ oauth/main.js
@@ -11,8 +11,10 @@ const {app} = require("electron");
 var Asana = require('asana');
 const path = require('path')
 const url = require('url')
-const etray = require("./main/tray")
+const etray = require("./main/tray_proj")
+const tokenStore = require('./token-store.json');
 const envVariables = require('./env-variables.json');
+const fs = require('fs')
 
 // Create a client, getting parameters from the environment.
 
@@ -22,32 +24,65 @@ const envVariables = require('./env-variables.json');
 // When `authorize` is called it will prompt us to perform the authorization
 // in a browser and copy in the code we got. It will then exchange that for
 // a token.
-console.log("clientid = " + envVariables.ASANA_CLIENT_ID);
-const client = Asana.Client.create({
-  clientId: envVariables.ASANA_CLIENT_ID,//process.env['ASANA_CLIENT_ID'],
-  clientSecret: envVariables.ASANA_CLIENT_SECRET//process.env['ASANA_CLIENT_SECRET']
-});
+//console.log("clientid = " + envVariables.ASANA_CLIENT_ID);
+//const client = Asana.Client.create({
+//  clientId: envVariables.ASANA_CLIENT_ID,//process.env['ASANA_CLIENT_ID'],
+//  clientSecret: envVariables.ASANA_CLIENT_SECRET//process.env['ASANA_CLIENT_SECRET']
+//});
 
-function refreshAuth() {
-  // refresh token from previous authorisation
-  // examples/oauth/webserver
-  const credentials = {
-  // access_token: 'my_access_token',
-  refresh_token: 'my_refresh_token'
-};
-client.useOauth({
-  credentials: credentials
-});
+function createClient() {
+  return Asana.Client.create({
+    clientId: envVariables.ASANA_CLIENT_ID,
+    clientSecret: envVariables.ASANA_CLIENT_SECRET
+  });
 }
 
-function authorizeAsana() {
-  client.useOauth();
-  return client.authorize();
+
+function getClient() {
+  var client = createClient();
+  var token = tokenStore.token;
+  if (token) {
+    // Here's where we direct the client to use Oauth with the credentials
+    // we have acquired.
+    console.log("attempting to authenticate with: " + token);
+    client.useOauth({ credentials: token });
+    client.users.me().then(function(me) {
+      console.log('Hello ' + me.name);
+      return client
+    }).catch(function(err) {
+      console.log('Error fetching user: ' + err);
+    });
+  } else {
+    // Otherwise redirect to authorization.
+      console.log("No token found go to auth url:");
+      client.useOauth();
+      client.authorize().then(function() {
+      console.log("authorized refresh_token: ");
+      console.log(client.dispatcher.authenticator.credentials);
+      var jsonVariable = {'token': client.dispatcher.authenticator.credentials};
+      fs.writeFileSync('./token-store.json', JSON.stringify(jsonVariable));
+      return client;
+    });
+  }
+  return client
 }
+
+function authorizeAsana(){
+  let promise = new Promise(function(resolve, reject) {
+              var client = getClient();
+              console.log("client")
+              resolve(client)
+  });
+  return promise
+}
+
 
 async function showTray() {
+  let win;
   try {
-    await authorizeAsana();
+    //var client = 0;
+    var client = await authorizeAsana();
+    console.log("authorised---> ");
     return etray.buildTray(client);
   } catch (err) {
     console.log("app not authorised yet");
