536d4f651392e369b1439fb742a2b07c8f996d91
domwhill <dominicwhill@gmail.com>
2021-06-25 21:35:12 +0100

    Use electron store instead of writing to json

--- oauth/window.js
+++ oauth/window.js
@@ -1,5 +1,14 @@
-      const fs = require('fs')
-      const projectGids = require('./project_gids.json');
+      //const fs = require('fs')
+      //const fs = require('browserify-fs')
+      const Store = require('electron-store');
+      const schema = {gid: { type:
+                        'string',
+                         maximum: 100,
+                         minimum: 1,
+                         default:["1137023841060961","1121178260222489"]}}
+      const store = new Store(schema);
+
+      //const projectGids = require('./project_gids.json');
 
       const allProjects = document.querySelector('.allProjects');
       const projectFieldSubmit = document.querySelector('.projectFieldSubmit');
@@ -7,21 +16,21 @@
       const list = document.querySelector('.output ul');
       allProjects.textContent = 'test content';
       list.innerHTML = '';
-
-      var gids = projectGids.gid;
-      console.log(gids)
+      var gids = store.get('gid')
+      //var gids = projectGids.gid;
+      //console.log(gids)
 
       function writeList(in_gid){
       var gid_str = '';
       let i=0;
       list.innerHTML = '';
       do {
-        i =i+1;
-        console.log(in_gid[i]);
-        gid_str += in_gid[i] + '\n';
-        let listItem = document.createElement('li');
-        listItem.textContent = in_gid[i];
-        list.appendChild(listItem);
+            i =i+1;
+            console.log(in_gid[i]);
+            gid_str += in_gid[i] + '\n';
+            let listItem = document.createElement('li');
+            listItem.textContent = in_gid[i];
+            list.appendChild(listItem);
       } while (i < in_gid.length -1)
       }
 
@@ -35,7 +44,7 @@
         console.log("gid_json")
         console.log(gid_json)
         writeList(gids)
-        fs.writeFileSync('../project_gids_bkup.json', JSON.stringify(gid_json));
+        store.set('gid', gids)
       }
 
       writeList(gids)
