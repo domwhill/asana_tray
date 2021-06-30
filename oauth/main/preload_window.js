a124b6576cfb455de9e8411126b1aeef3d1497a4
domwhill <dominicwhill@gmail.com>
2021-07-01 00:21:10 +0100

    Move store into function in preload.js

--- oauth/main/preload_window.js
+++ oauth/main/preload_window.js
@@ -1,23 +1,28 @@
   const { contextBridge, ipcRenderer} = require('electron')
   const Store = require('electron-store');
-  const schema = {gid: { type:
-                    'string',
-                     maximum: 100,
-                     minimum: 1,
-                     default:["1137023841060961","1121178260222489"]}}
-  const store = new Store(schema);
 
 
 
-  function get_gid(){
+  function get_gid_store() {
+    const schema = {gid: { type:
+                      'string',
+                       maximum: 100,
+                       minimum: 1,
+                       default:["1137023841060961","1121178260222489"]}}
+
+    const store = new Store(schema);
    console.log(window.store.get('gid'))
    return window.store.get('gid')
   }
 
 
+
 contextBridge.exposeInMainWorld(
     'api',
     {
-        getgid: () => store.get('gid')
+        getgid: () => ({
+            data: get_gid_store
+        }),
+        get_gid: () => ipcRenderer.send('do-a-thing')
     }
 )
